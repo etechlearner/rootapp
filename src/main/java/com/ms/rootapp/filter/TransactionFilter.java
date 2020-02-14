f7f9091cf56fbc98b819fd97f1906c45138a3937
tahir.appiskey <tahir.saleem@appiskey.com>
2020-02-15 00:44:27 +0500

    health check disabled

--- src/main/java/com/ms/rootapp/filter/TransactionFilter.java
+++ src/main/java/com/ms/rootapp/filter/TransactionFilter.java
@@ -26,7 +26,7 @@ public class TransactionFilter implements Filter {
 
         String requestURL = String.valueOf(req.getRequestURL());
 
-        if (!requestURL.endsWith(extension+"/")){
+        if (!requestURL.endsWith(extension+"/") && !requestURL.endsWith("health/")){
             res.sendRedirect(requestURL+"/");
         }
         logger.info(
